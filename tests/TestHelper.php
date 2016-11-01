49115e4f1d200db6e15f7c0fbaadb30b7a877b19
wings <hotman8168@gmail.com>
2016-11-02 00:10:29 +0800

    引入phpunit

---
+++ tests/TestHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * User: xingjun
+ * Date: 2016/11/1
+ * Time: 23:56
+ */
+
+use Phalcon\Di;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+define("ROOT_PATH", __DIR__);
+
+set_include_path(
+    ROOT_PATH . PATH_SEPARATOR . get_include_path()
+);
+
+// Required for phalcon/incubator
+include __DIR__ . "/../vendor/autoload.php";
+
+// Use the application autoloader to autoload the classes
+// Autoload the dependencies found in composer
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        ROOT_PATH,
+    ]
+);
+
+$loader->register();
+
+$di = new FactoryDefault();
+
+Di::reset();
+
+// Add any needed services to the DI here
+
+Di::setDefault($di);
